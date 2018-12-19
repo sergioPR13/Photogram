133fb7c6ee7d210ef3f45e9c70abaafc3e0b55f9
sergio <srgpr13@gmail.com>
2018-12-19 12:11:25 -0800

    Crear comentarios con AJAX

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
